71b8888f59e70e6239d079c99c426c4df62e2ae3
Vinicius Tocchio <80677157+viniciustocchio@users.noreply.github.com>
2022-08-31 09:18:38 +0200

    added hooks

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+export function LoginView(props) {
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+const handleSubmit = (e) => {
+  e.preventDEfault();
+  console.log(username, password);
+};
+
+  retun (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+}
