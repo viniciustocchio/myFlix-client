24c7fd966b9aff0bf7bd7be463e4484c597c3300
vtocchio <80677157+viniciustocchio@users.noreply.github.com>
2022-09-06 16:26:38 +0200

    small changes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import axios from "axios";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
-import { LoginView } from "../login-view/login-view";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { RegistrationView } from '../registration-view/RegistrationView';
+
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export class MainView extends React.Component {
   constructor() {
@@ -19,10 +23,6 @@ export class MainView extends React.Component {
 
   componentDidMount() {
     let accessToken = localStorage.getItem("token");
-    if (accessToken !== null) {
-      this.setState({
-        user: localStorage.getItem("user"),
-      });
       this.getMovies(accessToken);
     }
   }
@@ -67,46 +67,49 @@ export class MainView extends React.Component {
     });
   }
 
-  render() {
-    const { movies, selectedMovie } = this.state;
+render() {
+  const { movies, user } = this.state;
 
-    // if (!user)
-    //   return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
+  if (!user) return <Row>
+    <Col>
+      <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+    </Col>
+  </Row>
+  if (movies.length === 0) return <div className="main-view" />;
 
-    if (movies.length === 0) return <div className="main-view" />;
-
-    return (
+  return (
+    <Router>
       <Row className="main-view justify-content-md-center">
-        {selectedMovie ? (
-          <Col md={8}>
-            <MovieView
-              movie={selectedMovie}
-              onBackClick={(newSelectedMovie) => {
-                this.setSelectedMovie(newSelectedMovie);
-              }}
-            />
-          </Col>
-        ) : (
-          movies.map((movie) => (
-            <Col md={3}>
-              <MovieCard
-                key={movie._id}
-                movie={movie}
-                onMovieClick={(newSelectedMovie) => {
-                  this.setSelectedMovie(newSelectedMovie);
-                }}
-              />
+        <Route exact path="/" render={() => {
+          return movies.map(m => (
+            <Col md={3} key={m._id}>
+              <MovieCard movie={m} />
             </Col>
           ))
-        )}
-        <button
-          inClick={() => {
-            this.onLoggedOut();
-          }}
-        >
-          Logout
-        </button>
+        }} />
+        <Route path="/movies/:movieId" render={({ match }) => {
+          return <Col md={8}>
+            <MovieView movie={movies.find(m => m._id === match.params.movieId)} />
+          </Col>
+        }} />
+
       </Row>
-    );
-  }
+    </Router>
+  );
 }
+}
+
+<Router>
+   <div className="main-view">
+    <Route exact path="/" render={/* welcome */}/>
+    <Route exact path="/movies/:movieId" render={/* movie view */}/>
+    <Route exact path="/genres/:name" render={/* genre view*/}/>
+    <Route exact path="/directors/:name" render={({ match }) => {
+  if (movies.length === 0) return <div className="main-view" />;
+  return <Col md={8}>
+    <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director} />
+  </Col>
+}
+} />
+   </div>
+</Router>
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+import { Link } from "react-router-dom";
 
 export class MovieCard extends React.Component {
   render() {
@@ -13,7 +15,9 @@ export class MovieCard extends React.Component {
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
-          <Button onClick={() => onMovovieClick(movie)} variant="link">Open</Button>
+          <Button onClick={() => onMovovieClick(movie)} variant="link">
+            Open
+          </Button>
         </Card.Body>
       </Card>
     );
